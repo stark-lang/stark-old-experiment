6f249324ae73bb26b2ffdaf593673071e1e874f6
Alexandre Mutel <alexandre_mutel@live.com>
2017-02-16 23:44:28 +0100

    Add QueueArray utility class

---
+++ src/compiler/Stark.Compiler/Collections/QueueArray.cs
@@ -0,0 +1,404 @@
+﻿// Copyright (c) The Stark Programming Language Contributors. All rights reserved.
+// Licensed under the MIT license. 
+// See license.txt file in the project root for full license information.
+
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+
+namespace Stark.Compiler.Collections
+{
+    // A simple Queue of generic objects.  Internally it is implemented as a 
+    // circular buffer, so Enqueue can be O(n).  Dequeue is O(1).
+    // This is a derivation of corefx Queue.cs that adds only the this[index] method
+    //[DebuggerTypeProxy(typeof(ListDebugView<>))]
+    [DebuggerDisplay("Count = {Count}")]
+    public class QueueArray<T> : IReadOnlyCollection<T>
+    {
+        private T[] _array;
+        private int _head;       // The index from which to dequeue if the queue isn't empty.
+        private int _tail;       // The index at which to enqueue if the queue isn't full.
+        private int _size;       // Number of elements.
+
+        private const int MinimumGrow = 4;
+        private const int GrowFactor = 200;  // double each time
+
+        // Creates a queue with room for capacity objects. The default initial
+        // capacity and grow factor are used.
+        public QueueArray()
+        {
+            _array = Array.Empty<T>();
+        }
+
+        // Creates a queue with room for capacity objects. The default grow factor
+        // is used.
+        public QueueArray(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity), capacity, "Capacity cannot be negative");
+            _array = new T[capacity];
+        }
+
+        public int Count => _size;
+
+        // Removes all Objects from the queue.
+        public void Clear()
+        {
+            if (_size != 0)
+            {
+                if (_head < _tail)
+                    Array.Clear(_array, _head, _size);
+                else
+                {
+                    Array.Clear(_array, _head, _array.Length - _head);
+                    Array.Clear(_array, 0, _tail);
+                }
+
+                _size = 0;
+            }
+
+            _head = 0;
+            _tail = 0;
+        }
+
+        // CopyTo copies a collection into an Array, starting at a particular
+        // index into the array.
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (arrayIndex < 0 || arrayIndex > array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), arrayIndex, "index out of range");
+            }
+
+            int arrayLen = array.Length;
+            if (arrayLen - arrayIndex < _size)
+            {
+                throw new ArgumentException("Invalid array length");
+            }
+
+            int numToCopy = _size;
+            if (numToCopy == 0) return;
+
+            int firstPart = Math.Min(_array.Length - _head, numToCopy);
+            Array.Copy(_array, _head, array, arrayIndex, firstPart);
+            numToCopy -= firstPart;
+            if (numToCopy > 0)
+            {
+                Array.Copy(_array, 0, array, arrayIndex + _array.Length - _head, numToCopy);
+            }
+        }
+
+        // Adds item to the tail of the queue.
+        public void Enqueue(T item)
+        {
+            if (_size == _array.Length)
+            {
+                int newcapacity = (int)((long)_array.Length * (long)GrowFactor / 100);
+                if (newcapacity < _array.Length + MinimumGrow)
+                {
+                    newcapacity = _array.Length + MinimumGrow;
+                }
+                SetCapacity(newcapacity);
+            }
+
+            _array[_tail] = item;
+            MoveNext(ref _tail);
+            _size++;
+        }
+
+        // GetEnumerator returns an IEnumerator over this Queue.  This
+        // Enumerator will support removing.
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        /// <internalonly/>
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        // Removes the object at the head of the queue and returns it. If the queue
+        // is empty, this method throws an 
+        // InvalidOperationException.
+        public T Dequeue()
+        {
+            if (_size == 0)
+            {
+                ThrowForEmptyQueue();
+            }
+
+            T removed = _array[_head];
+            _array[_head] = default(T);
+            MoveNext(ref _head);
+            _size--;
+            return removed;
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            if (_size == 0)
+            {
+                result = default(T);
+                return false;
+            }
+
+            result = _array[_head];
+            _array[_head] = default(T);
+            MoveNext(ref _head);
+            _size--;
+            return true;
+        }
+
+        // Returns the object at the head of the queue. The object remains in the
+        // queue. If the queue is empty, this method throws an 
+        // InvalidOperationException.
+        public T Peek()
+        {
+            if (_size == 0)
+            {
+                ThrowForEmptyQueue();
+            }
+
+            return _array[_head];
+        }
+
+        public bool TryPeek(out T result)
+        {
+            if (_size == 0)
+            {
+                result = default(T);
+                return false;
+            }
+
+            result = _array[_head];
+            return true;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), index, "Index cannot be negative");
+                }
+
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), index, "Cannot peek more than the elements in the queue");
+                }
+
+                var fetchIndex = _head + index;
+                if (fetchIndex >= _array.Length)
+                {
+                    fetchIndex -= _array.Length;
+                }
+                return _array[fetchIndex];
+            }
+        }
+
+        // Returns true if the queue contains at least one object equal to item.
+        // Equality is determined using EqualityComparer<T>.Default.Equals().
+        public bool Contains(T item)
+        {
+            if (_size == 0)
+            {
+                return false;
+            }
+
+            if (_head < _tail)
+            {
+                return Array.IndexOf(_array, item, _head, _size) >= 0;
+            }
+
+            // We've wrapped around. Check both partitions, the least recently enqueued first.
+            return
+                Array.IndexOf(_array, item, _head, _array.Length - _head) >= 0 ||
+                Array.IndexOf(_array, item, 0, _tail) >= 0;
+        }
+
+        // Iterates over the objects in the queue, returning an array of the
+        // objects in the Queue, or an empty array if the queue is empty.
+        // The order of elements in the array is first in to last in, the same
+        // order produced by successive calls to Dequeue.
+        public T[] ToArray()
+        {
+            if (_size == 0)
+            {
+                return Array.Empty<T>();
+            }
+
+            T[] arr = new T[_size];
+
+            if (_head < _tail)
+            {
+                Array.Copy(_array, _head, arr, 0, _size);
+            }
+            else
+            {
+                Array.Copy(_array, _head, arr, 0, _array.Length - _head);
+                Array.Copy(_array, 0, arr, _array.Length - _head, _tail);
+            }
+
+            return arr;
+        }
+
+        // PRIVATE Grows or shrinks the buffer to hold capacity objects. Capacity
+        // must be >= _size.
+        private void SetCapacity(int capacity)
+        {
+            T[] newarray = new T[capacity];
+            if (_size > 0)
+            {
+                if (_head < _tail)
+                {
+                    Array.Copy(_array, _head, newarray, 0, _size);
+                }
+                else
+                {
+                    Array.Copy(_array, _head, newarray, 0, _array.Length - _head);
+                    Array.Copy(_array, 0, newarray, _array.Length - _head, _tail);
+                }
+            }
+
+            _array = newarray;
+            _head = 0;
+            _tail = (_size == capacity) ? 0 : _size;
+        }
+
+        // Increments the index wrapping it if necessary.
+        private void MoveNext(ref int index)
+        {
+            // It is tempting to use the remainder operator here but it is actually much slower 
+            // than a simple comparison and a rarely taken branch.   
+            int tmp = index + 1;
+            index = (tmp == _array.Length) ? 0 : tmp;
+        }
+
+        private void ThrowForEmptyQueue()
+        {
+            Debug.Assert(_size == 0);
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        public void TrimExcess()
+        {
+            int threshold = (int)(((double)_array.Length) * 0.9);
+            if (_size < threshold)
+            {
+                SetCapacity(_size);
+            }
+        }
+
+        // Implements an enumerator for a Queue.  The enumerator uses the
+        // internal version number of the list to ensure that no modifications are
+        // made to the list while an enumeration is in progress.
+        [SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes", Justification = "not an expected scenario")]
+        [Serializable]
+        public struct Enumerator : IEnumerator<T>,
+            System.Collections.IEnumerator
+        {
+            private readonly QueueArray<T> _q;
+            private int _index;   // -1 = not started, -2 = ended/disposed
+            private T _currentElement;
+
+            internal Enumerator(QueueArray<T> q)
+            {
+                _q = q;
+                _index = -1;
+                _currentElement = default(T);
+            }
+
+            public void Dispose()
+            {
+                _index = -2;
+                _currentElement = default(T);
+            }
+
+            public bool MoveNext()
+            {
+                if (_index == -2)
+                    return false;
+
+                _index++;
+
+                if (_index == _q._size)
+                {
+                    // We've run past the last element
+                    _index = -2;
+                    _currentElement = default(T);
+                    return false;
+                }
+
+                // Cache some fields in locals to decrease code size
+                T[] array = _q._array;
+                int capacity = array.Length;
+
+                // _index represents the 0-based index into the queue, however the queue
+                // doesn't have to start from 0 and it may not even be stored contiguously in memory.
+
+                int arrayIndex = _q._head + _index; // this is the actual index into the queue's backing array
+                if (arrayIndex >= capacity)
+                {
+                    // NOTE: Originally we were using the modulo operator here, however
+                    // on Intel processors it has a very high instruction latency which
+                    // was slowing down the loop quite a bit.
+                    // Replacing it with simple comparison/subtraction operations sped up
+                    // the average foreach loop by 2x.
+
+                    arrayIndex -= capacity; // wrap around if needed
+                }
+
+                _currentElement = array[arrayIndex];
+                return true;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (_index < 0)
+                        ThrowEnumerationNotStartedOrEnded();
+                    return _currentElement;
+                }
+            }
+
+            private void ThrowEnumerationNotStartedOrEnded()
+            {
+                Debug.Assert(_index == -1 || _index == -2);
+                throw new InvalidOperationException(_index == -1 ? "Queue Iterator not started" : "Queue Iterator already ended");
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            void IEnumerator.Reset()
+            {
+                _index = -1;
+                _currentElement = default(T);
+            }
+        }
+    }
+}
